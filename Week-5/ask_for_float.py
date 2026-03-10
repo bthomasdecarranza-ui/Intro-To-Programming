f6d6f54fb42280209178f56f64e42e3199b92402
bthomasdecarranza-ui <bthomasdecarranza@berklee.edu>
2026-03-09 23:26:59 -0400

    float

---
+++ Week-5/ask_for_float.py
@@ -0,0 +1,9 @@
+# Ask the user for a number
+user_input = input("Enter a floating-point number: ")
+
+# Convert the input to a float
+number = float(user_input)
+
+# Print the value and its type
+print(number)
+print(type(number))
