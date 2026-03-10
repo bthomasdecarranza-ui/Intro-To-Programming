0a0a23df99e49c765bbaf4a45ea06cb1e2031225
bthomasdecarranza-ui <bthomasdecarranza@berklee.edu>
2026-03-09 23:22:55 -0400

    arguments

---
+++ Week-5/arguments.py
@@ -0,0 +1,25 @@
+# 1
+print(1,2,3,4,5,6,7,8,9,10)
+
+# 2
+print()  # This prints a blank line because print() with no arguments outputs just a newline.
+
+# 3
+# If you give pow() only one argument, Python will give an error because pow() requires at least two arguments.
+
+result = round(3.14159, 2)
+
+# 4
+# Function name: round
+
+# 5
+# It is being CALLED
+
+# 6
+# It takes 2 arguments
+
+# 7
+# The arguments are 3.14159 and 2
+
+# 8
+# Yes, it returns a value. We know because the result is stored in the variable "result".
