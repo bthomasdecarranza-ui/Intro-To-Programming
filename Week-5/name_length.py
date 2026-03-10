e3a3dcd027e0732cca942a7966f17296d437f574
bthomasdecarranza-ui <bthomasdecarranza@berklee.edu>
2026-03-09 23:29:03 -0400

    name

---
+++ Week-5/name_length.py
@@ -0,0 +1,9 @@
+# Ask the user for their full name
+name = input("Enter your full name: ")
+
+# Calculate the length of the name
+length = len(name)
+
+# Print the length and its type
+print(length)
+print(type(length))
