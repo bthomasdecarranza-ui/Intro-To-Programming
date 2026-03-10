efcccf1e5b84c165fe49eaf571ee0a2fa32f6065
bthomasdecarranza-ui <bthomasdecarranza@berklee.edu>
2026-03-09 23:12:07 -0400

    sd

--- Week-4/example.py
+++ Week-4/example.py
@@ -1,16 +1,2 @@
-# This is a COMMENT - it is for humans
-# The left hand side is the variable name, right hand side is the value
-
-'''
-This is a multi line comment
-Name: Beltran
-Class: Intro to Programming
-Date: Feb 10, 2026
-'''
-
-name = "Beltran"
-last_name = "Thomas de Carranza"
-
-# Print is a function that takes in arguments and prints them in terminal.
-print(name, last_name)
-
+x = 3.0
+print(type(x))
